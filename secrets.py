40d4b6e2ae28d9299d0d05d56d9f9a38703d1e3b
David Glaude <david.glaude@gmail.com>
2020-01-29 21:48:30 +0100

    Fake credential, you should put your Wifi credential.

---
+++ secrets.py
@@ -0,0 +1,8 @@
+secrets = {
+    'ssid' : 'SSID',
+    'password' : 'PASSWORD',
+    'timezone' : "Europe/Brussels", # http://worldtimeapi.org/timezones
+    'aio_username' : 'dglaude',
+    'aio_key' : 'abcd'
+    }
+
